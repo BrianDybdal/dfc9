fd46a4915ee06396f2c3e8dca17c6237722a5034
BrianDybdal <94491495+BrianDybdal@users.noreply.github.com>
2021-11-17 00:13:27 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -20,9 +20,57 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-    {
+      {
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Frederik Pedersen",
+      },
+      {
+      "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Jakub Droppa",
+      },
+      {
+      "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Sebastian V. Laursen",
+      },
+      {
+      "roster": 4,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Bjarke Wanstrup",
+      },
+      {
+      "roster": 5,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Jesper Hamborg",
+      },
+      {
+      "roster": 6,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Mikkel Guldbjerg Jensen",
+      },
+      {
+      "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Carsten Benn",
+      },
+      {
+      "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Klaus Avnegaard Henriksen",
+      },
+      {
+      "roster": 9,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Lars Jedermann",
+      },
+      {
+      "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Mikkel Sundtoft",
+      },
+      {
+      "roster": 11,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Jonathan BL",
+      },
+      {
+      "roster": 12,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "Anders Bank-Rasmussen",
+      },  
+//     {
+//       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+//       "name": "Frederik Pedersen",
 //       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
 //       "location": "null", // (optional)
 //       "bio": "null",
@@ -34,14 +82,14 @@ export const managers = [
 //         name: "Rival", // Can be anything (usually your rival's name)
 //         link: 6, // manager array number within this array, or null to link back to all managers page
 //         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      },
+//       },
 //       "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
 //       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
 //       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
 //       "philosophy": "Your fantasy team's philosophy",
 //       "tradingScale": 10, // 1 - 10
 //       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    },
+//     },
 //     {
 //       "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
 //       "name": "Your Name",
